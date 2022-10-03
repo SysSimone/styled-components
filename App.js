bcff46310970df3c51fa7f731bf8a94c14d719fc
SysSimone <developer.desenv@gmail.com>
2022-10-03 17:14:36 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { View, StyleSheet, StatusBar } from "react-native";
+import CheckBox from "./src/components/CheckBox";
+
+export default function App() {
+  const [check, setCheck] = useState(false)
+  function handleCheck() {
+    setCheck(!check);
+  }
+  return (
+    <>
+      <StatusBar backgroundColor="#33ccff" />
+      <View style={styles.Container}>
+
+        {/* Esse é o nosso lindo checkBox */}
+        {/* <CheckBox
+          label="Esse é o checkbox"
+          labelStyle={{ color: '#fff', fontSize: 16 }}
+          iconColor="#fff"
+          checkColor="#fff"
+          value={check}
+          onChange={handleCheck}
+        /> */}
+
+         <CheckBox
+          label="Esse é o checkbox"
+          labelStyle={{ color: 'black', fontSize: 16 }}
+          iconColor="black"
+          checkColor="black"
+          value={check}
+          onChange={handleCheck}
+        /> 
+      </View>
+    </>
+  )
+}
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "baseline",
+    backgroundColor: '#33ccff',
+    padding: 20
+  }
+})
---
+++ src/components/CheckBox.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import  PropTypes  from "prop-types";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+export default function CheckBox(props) {
+  function handleChange() {
+    const { onChange } = props;
+    if (onChange) {
+      return onChange();
+    }
+  }
+
+  return (
+    <View style={styles.WrapperCheckBox}>
+
+      <TouchableOpacity onPress={handleChange} style={[
+        styles.CheckBox,
+        { borderColor: props.checkColor ? props.checkColor : '#fff' }
+      ]}>
+        {
+          props.value ? <Icon name="check"
+            style={{
+              fontSize: 16,
+              color: props.iconColor ? props.iconColor : '#fff'
+            }}
+          /> : null
+        }
+
+      </TouchableOpacity>
+
+      <Text style={[styles.LabelCheck, props.labelStyle]}>
+        {props.label}
+      </Text>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  CheckBox: {
+    width: 25,
+    height: 25,
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  WrapperCheckBox: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  LabelCheck: {
+    color: '#fff',
+    marginLeft: 6
+  }
+});
+
+
+CheckBox.propTypes = {
+  label: PropTypes.string,
+  labelStyle: PropTypes.object,
+  iconColor: PropTypes.string,
+  onChange: PropTypes.func,
+  value: PropTypes.bool,
+  checkColor: PropTypes.string,
+};
+
+//  Nosso componente será basicamente um quadrado vazado e ao lado desse
+//  quadrado uma label. Ao clicar no quadrado (que está vazado no momento), 
+//  terá que aparecer um check e vice-versa. Precisamos então de um botão, algumas views, 
+//  texto, ícones e estilo.
